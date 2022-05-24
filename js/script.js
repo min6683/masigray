c00f489b6d8a8dc0a9c82c83f4fc54d650f445a3
min6683 <dev.sm6683@gmail.com>
2022-05-24 13:08:50 +0900

    헤더 비주얼 반응형 작업

---
+++ js/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    // 주메뉴 모션 적용
+    let nav = $('.nav');
+    let headerMain = $('.header-main');
+    let logo = $('.logo');
+    nav.mouseenter(function(){
+        $(this).addClass('nav-active');
+    });
+    headerMain.mouseleave(function(){
+        nav.removeClass('nav-active');
+    });
+    logo.mouseenter(function(){
+        nav.removeClass('nav-active');
+    });
+
+    // 스크롤 기능
+    let header = $('.header')
+    $(window).scroll(function(){
+        let temp = $(window).scrollTop();
+        if(temp > 0) {
+            header.addClass('fix-active')
+        }else{
+            header.removeClass('fix-active')
+        }
+    });
+    // 모바일 메뉴
+    let mBt = $('.m-bt');
+    let mMenuBg = $('.m-menu-bg');
+    let mMenuWrap = $('.m-menu-wrap');
+    let mBtClose = $('.m-bt-close');
+    mBt.click(function(){
+        mMenuWrap.addClass('m-menu-wrap-active');
+        mMenuBg.addClass('m-menu-bg-active');
+
+    });
+    mBtClose.click(function(){
+        mMenuWrap.removeClass('m-menu-wrap-active');
+        mMenuBg.removeClass('m-menu-bg-active');
+    });
+    mMenuBg.click(function(){
+        mMenuWrap.removeClass('m-menu-wrap-active');
+        mMenuBg.removeClass('m-menu-bg-active');
+        // event.stopPropagation();
+    });
+
+    // 화면 리사이징 처리
+    $(window).resize(function(){
+        let w = $(window).width();
+        if(w > 700) {
+            mMenuWrap.removeClass('m-menu-wrap-active');
+            mMenuBg.removeClass('m-menu-bg-active');
+        }
+    })
+    
+    
+    
+});
+window.onload = function(){
+    let swVisual = new Swiper('.sw-visual', {
+        loop:true,
+        navigation: {
+            prevEl: '.sw-visual-prev',
+            nextEl: '.sw-visual-next'
+        }
+    });
+}
